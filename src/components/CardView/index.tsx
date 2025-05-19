4a986195b26bcf434139a3f0f5d6b53d2f02f807
Ramon Noleto <devnoleto@gmail.com>
2025-05-19 15:56:43 -0300

    :tada: feat: Criação de componente Divider

--- src/components/CardView/index.tsx
+++ src/components/CardView/index.tsx
@@ -4,14 +4,28 @@ import Logo from "../../../assets/logo.png";
 
 
 import { styles } from "./style";
+import Divider from "../Divider";
 
 export default function CardView(){
+
+    const renderLogoBox = () => (
+        <View style={styles.logoContainer}>
+            <Image style={styles.imageLogo} source={Logo}/>
+        </View>
+    );
+
+    const renderCarDetails = () => (
+        <View style={{alignItems: "center"}}>
+            <Text style={styles.carBrand}>Lamborghini</Text>
+            <Text style={styles.carName}>Modelo</Text>
+        </View>
+    )
     
     return(
         <View style={styles.imageContainer}>
-            <View style={styles.logoContainer}>
-                <Image style={styles.imageLogo} source={Logo}/>
-            </View>
+            {renderLogoBox()}
+            <Divider />
+            {renderCarDetails()}
         </View>
     )
 
--- src/components/CardView/style.tsx
+++ src/components/CardView/style.tsx
@@ -17,8 +17,21 @@ export const styles = StyleSheet.create({
         resizeMode: "contain",
     },
     imageLogo:{
+        marginTop:10,
         flex:1,
         width:"65%",
         resizeMode:"contain",
+    },
+    carBrand:{
+        color:"#fff",
+        fontWeight: "400",
+        fontSize: 18,
+        fontStyle: "italic",
+    },
+    carName:{
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
     }
 })
