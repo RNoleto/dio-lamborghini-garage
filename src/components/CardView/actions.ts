4c4a454ddbc26c8279aa6a8e36a1b8d4ae0cd19b
Ramon Noleto <devnoleto@gmail.com>
2025-06-06 15:24:46 -0300

    :tada: feat: Load functions finalizada

--- src/components/CardView/actions.ts
+++ src/components/CardView/actions.ts
@@ -1,3 +1,4 @@
+import React from "react";
 import { fetchGetCarData } from "../../api/getCars";
 import { CarModel } from "./props";
 
@@ -14,10 +15,46 @@ export const loadCarData = async(id:number, setCarData: React.Dispatch<React.Set
     }
 };
 
-export const handlerPreviusItem = async() => {
+export const handlerPreviusItem = async(
+    carData: CarModel | null,
+    setCarData: React.Dispatch<React.SetStateAction<CarModel | null>>
+) => {
+    let response = null;
+    try {
+        if(carData && carData?.id > 1){
+            response = await fetchGetCarData(carData.id -1)
+        }
+        if(carData && carData?.id == 1){
+            response = await fetchGetCarData(carData.id = 10)
+        }
+        if(response){
+            setCarData(response);
+        }
 
+    } catch (error) {
+        console.log("Erro ao chamar a api", error);
+        setCarData(null);
+    }
 };
 
-export const handlerNextItem = async() => {
+export const handlerNextItem = async(
+    carData: CarModel | null,
+    setCarData: React.Dispatch<React.SetStateAction<CarModel | null>>
+) => {
+    let response = null;
+    try {
+        if(carData && carData?.id < 10){
+            response = await fetchGetCarData(carData.id +1)
+        }
+        if(carData && carData?.id == 10){
+            response = await fetchGetCarData(carData.id = 1);
+        }
+        if(response){
+            setCarData(response);
+        }
 
+    } catch (error) {
+        console.log("Erro ao chamar a api", error);
+        setCarData(null);
+    }
 };
